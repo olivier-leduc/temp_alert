9fe57f32b60587278b52e0cbdee4011b82b42597
olivier-leduc <pi@pizero.lan>
2019-11-10 15:00:33 -0800

    added google spreadsheet feature

---
+++ google_lib.py
@@ -0,0 +1,147 @@
+"""Uses Google APIs to send email and update spreadsheet.
+"""
+
+import base64
+import logging
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+
+#from __future__ import print_function
+import httplib2
+import os
+
+from googleapiclient import discovery
+from googleapiclient import errors
+import oauth2client.file
+from oauth2client import client
+from oauth2client import tools
+
+_BUILD = discovery.build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose', 'https://www.googleapis.com/auth/drive']
+CLIENT_SECRET_FILE = '/home/pi/credentials/client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def InitGoogleService(app, version, flags):
+    credentials =  get_credentials(flags)
+    http = credentials.authorize(httplib2.Http())
+    service = _BUILD(app, version, http=http)
+    return service
+
+
+def get_credentials(flags):
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('/home/pi/temp_alert')
+    credential_dir = os.path.join(home_dir, 'credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'google_creds.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def SendMessage(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def CreateMessage(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def AppendGsheet(service, values, sheet_id):
+  """Append rows to Google spreadsheet.
+
+  Args:
+    credentials:  oauth2 credentials for accessing the Drive API.
+    sheet_id: A string, Titleid of the Gsheet
+  Returns:
+  """
+  request = service.spreadsheets().values().append(
+      spreadsheetId=sheet_id,
+      range='Sheet1!A1',
+      valueInputOption='RAW',
+      body={ 'values': values }
+  )
+  try:
+    response = request.execute()
+  except errors as e:
+    print e
+    raise
+  return response
+
+
+def ClearSheet(service, sheet_id):
+  """Clear rows from given Google spreadsheet.
+
+  Args:
+    credentials:  oauth2 credentials for accessing the Drive API.
+    sheet_id: A string, Titleid of the Gsheet
+  Returns:
+  """
+  range_all_rows = 'Sheet1!A2:Z'
+  request = service.spreadsheets().values().clear(
+      spreadsheetId=sheet_id,
+      range = range_all_rows,
+      body = {}
+      )
+  try:
+    response = request.execute()
+  except errors as e:
+    print e
+    raise
+  return response
+
+
--- temp_alert.py
+++ temp_alert.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python
-#This script now uses a Raspberry Pi zero W,
-#two DS18B20 temp sensor to monitor the freezer and room temperature.
+# !/usr/bin/python
+# This script uses a Raspberry Pi zero W and support up to 2x DS18B20
+# temperature sensors.
+#
+# A minimum of one sensor is needed for the script to startup.
+# temp_alert does the following:
+# 1) logs temperature into a local textfile and a Google spreadsheet.
+# 2) Sends an email when temperature goes out of bounds.
 
 from datetime import datetime
-from pytz import timezone
 from time import sleep, strftime, time
 from decimal import Decimal
 
@@ -12,77 +16,79 @@ import pytz
 import os
 import glob
 import logging
-import signal
-import traceback
 import threading
-import gmail_lib as gmail
-toggledots = 0
+import google_lib
+from oauth2client import tools
 
-parser = argparse.ArgumentParser(description='blah')
+parser = argparse.ArgumentParser(parents=[tools.argparser], description='')
+parser.add_argument('--sheetid', dest='sheetid')
 parser.add_argument('--alertable', dest='alert', action='store_true')
+flags = parser.parse_args()
 
-args = parser.parse_args()
-
-# Global Variables
-LOWROOM = 62
-HIGHROOM = 99
-LOWFREEZER = -30
-HIGHFREEZER = 5
-TO_ADDRESS = 'oleduc-pager@google.com'
-FROM_ADDRESS = 'kenjileduc@gmail.com'
-MESSAGE = ''
-WAIT_TIME = 3600
-
-
+# Temperature bounds.
+ROOM_TEMP_MIN_MAX = [62, 99]
+FREEZER_TEMP_MIN_MAX = [-30, 5]
+# Path of local CSV file.
+OUTPUT_FILE = "/home/pi/freezer_room_temp.csv"
+# Max number of rows in Google spreadsheet, before sheet gets cleared.
+# Each row is a temperature measurement cycle (ref. TEMP_WAIT)
+MAX_ROWS = 288
+# Email alert addresses.
+TO_ADDRESS = 'dstemailaddress'
+FROM_ADDRESS = 'srcemailaddress'
+# Time to wait between each temperature measurement.
+TEMP_WAIT = 150
+# Time to wait after temperature reached bound limit.
+ALERT_WAIT = 3600
 
 # Timer thread will kick off every xx seconds and measure the temperature
-class TimerClass(threading.Thread):
-    def __init__(self, device_file, device_file_two, single_probe=None):
+class TempAlert(threading.Thread):
+    def __init__(self, sensor_path_list):
         threading.Thread.__init__(self)
         self.event = threading.Event()
-        self.device_file = device_file
-        self.device_file_two = device_file_two
-        self.single_probe = single_probe
+        self.sensor_path_list = sensor_path_list
 
     def run(self):
-        # set the starttime
-        t0 = time() - WAIT_TIME
+        t0 = time() - ALERT_WAIT  # set the starttime
         while not self.event.is_set():
-	    with open("/home/pi/freezer_room_temp.csv", "a") as log:
-	            roomTemp = self.read_temp(1) # sensor 1 is the Room unit
-                    freezerTemp = ''
-                    if not self.single_probe:
-	              freezerTemp = self.read_temp(2) # sensor 2 is the Freezer unit
-		    msg_time  = datetime.now(tz=pytz.timezone(("America/Los_Angeles")))
-	            log.write("{0},{1}, {2}\n".format(str(msg_time.strftime('%Y-%m-%d %H:%M:%S')),str(freezerTemp),str(roomTemp)))
-	            t1 = time()
-	            if t1 - t0 > WAIT_TIME and args.alert:
-	                if self.checkTempRanges(roomTemp, freezerTemp):
-	        	    # checkTempRanges returns True if temp is out of bounds
-	                    t0 = time()  # This make sure we wait until WAIT_TIME is complete until we we check temp again.
-	            	    time.sleep(2)
-	            self.event.wait( 150 )
+	    with open(OUTPUT_FILE, "a") as log:
+		roomTemp = self.read_temp(self.sensor_path_list[0])
+		freezerTemp = ''
+		if len(self.sensor_path_list) > 1:
+		    freezerTemp = self.read_temp(self.sensor_path_list[1])
+		msg_time  = datetime.now(tz=pytz.timezone(("America/Los_Angeles")))
+		log.write("{0}, {1}, {2}\n".format((msg_time.strftime('%Y-%m-%d %H:%M:%S')),str(freezerTemp),str(roomTemp)))
+		row = [[msg_time.strftime('%Y-%m-%d %H:%M:%S'), float(freezerTemp), float(roomTemp)]]
+		self.WriteToSheet(row)
+		t1 = time()
+		if t1 - t0 > ALERT_WAIT:
+		    if self.checkTempRanges(roomTemp, ROOM_TEMP_MIN_MAX, 'room'):
+			# checkTempRanges returns True if temp is out of bounds
+			t0 = time()  # This make sure we wait until ALERT_WAIT is complete until we check temp again and send an alert.
+			time.sleep(2)
+		    if freezerTemp:
+			if self.checkTempRanges(freezerTemp, FREEZER_TEMP_MIN_MAX, 'freezer'):
+			    # checkTempRanges returns True if temp is out of bounds
+			    t0 = time()  # This make sure we wait until ALERT_WAIT is complete until we check temp again and send an alert.
+			    time.sleep(2)
+		self.event.wait(TEMP_WAIT)
 
     def stop(self):
         self.event.set()
 
 
     # reads the actual files where the DS18B20 temp sensor records it.
-    def read_raw_temp(self, sense):
-        if sense == 1:
-          f = open(self.device_file, 'r')
-        else:
-          f = open(self.device_file_two, 'r')
-     
+    def read_raw_temp(self, sensor_path):
+        f = open(sensor_path, 'r')
         lines = f.readlines()
         f.close()
         return lines
     
-    def read_temp(self, sense):
-    	lines = self.read_raw_temp(sense)
+    def read_temp(self, sensor_path):
+    	lines = self.read_raw_temp(sensor_path)
     	while lines[0].strip()[-3:] != 'YES':
     	    time.sleep(0.1)
-    	    lines = self.read_raw_temp()
+    	    lines = self.read_raw_temp(sensor_path)
     	equals_pos = lines[1].find('t=')
     	if equals_pos != -1:
     	    temp_string = lines[1][equals_pos+2:]
@@ -90,54 +96,48 @@ class TimerClass(threading.Thread):
     	    temp_f = temp_c * 9.0 / 5.0 + 32.0
     	return str(temp_f)[:5]
     
-    def checkTempRanges(self, roomTemp, freezerTemp):
-         if Decimal(roomTemp) >= Decimal(HIGHROOM):
-           subject = '[Temperature Alert] : Room too warm! ' + str(roomTemp)
-           gmail.GmailWorkflow(FROM_ADDRESS, TO_ADDRESS, subject, MESSAGE)
-           print ("Temp too warm! Room: " + str(roomTemp)) 
-           return True
-         if freezerTemp:
-           if Decimal(freezerTemp) >= Decimal(HIGHFREEZER):
-             subject = '[Temperature Alert] : Freezer too warm! ' + str(freezerTemp)
-             gmail.GmailWorkflow(FROM_ADDRESS, TO_ADDRESS, subject, MESSAGE)
-             print ("Temp too warm! Freezer: " + str(freezerTemp)) 
-             return True
-         if Decimal(roomTemp) <= Decimal(LOWROOM):
-           subject = '[Temperature Alert] : Room too cold! ' + str(roomTemp)
-           gmail.GmailWorkflow(FROM_ADDRESS, TO_ADDRESS, subject, MESSAGE)
-           print ("Temp too cold! Room: " + str(roomTemp)) 
-           return True
-         if freezerTemp:
-           if Decimal(freezerTemp) <= Decimal(LOWFREEZER):
-             subject = '[Temperature Alert] : Freezer too cold! ' + str(freezerTemp)
-             gmail.GmailWorkflow(FROM_ADDRESS, TO_ADDRESS, subject, MESSAGE)
-             print ("Temp too cold! Freezer: " + str(freezerTemp)) 
-             return True
-         return
+    def checkTempRanges(self, temperature, temp_min_max, wherethesensoris):
+        message = ''
+        if Decimal(temperature) >= Decimal(temp_min_max[1]):
+	    cold_or_warm = 'warm! '
+        elif Decimal(temperature) <= Decimal(temp_min_max[0]):
+	    cold_or_warm = 'cold! '
+        else:
+          return False
+	print ('Temp too ' + cold_or_warm + str(temperature)) 
+        if flags.alert:
+	    subject = '[Temperature Alert] : ' + wherethesensoris + ' too  ' + cold_or_warm + str(temperature)
+	    self.SendEmailAlert(subject)
+	return True
+
+    def SendEmailAlert(subject):
+	service = google_lib.InitGoogleService('gmail', 'v1', flags)
+	email = google_lib.CreateMessage(TO_ADDRESS, FROM_ADDRESS, subject, message)
+	google_lib.SendMessage(service, "me", email)
+
+    def WriteToSheet(self, row):
+        service = google_lib.InitGoogleService('sheets', 'v4', flags)
+        sheet_content = service.spreadsheets().values().get(spreadsheetId=flags.sheetid, range="Sheet1!A2:B").execute().values()[1]
+        if len(sheet_content) > MAX_ROWS:
+	    logging.info('Reached cell %s in Google spreadsheet, clearing sheet', _MAX_ROWS)
+	    google_lib.ClearSheet(service, flags.sheetid)
+        google_lib.AppendGsheet(service, row, flags.sheetid)
 
 def main():
-  # need to be root to do this
-  os.system('modprobe w1-gpio')
-  os.system('modprobe w1-therm')
-  
-  base_dir = '/sys/bus/w1/devices/'
-  try:
-    single_probe = True
-    device_folder = glob.glob(base_dir + '28*')[0]
-    device_file = device_folder + '/w1_slave'
-    if len(glob.glob(base_dir + '28*')) > 1:
-      single_probe = None
-      device_folder_two = glob.glob(base_dir + '28*')[1]
-      device_file_two = device_folder_two + '/w1_slave'
-  except:
-    print "Error 23 reading temp sensor file, sensors connected?"
-    exit(23)  
-  
-  room_min_max = "Room: %dF-%dF" % (LOWROOM,HIGHROOM)
-  freezer_min_max = "\nFreezer:%dF-%dF" % (LOWFREEZER,HIGHFREEZER)
-  sleep(3)
-  tmr = TimerClass(device_file, device_file_two, single_probe=single_probe)
-  tmr.start()  # start the timer thread which will wake up and measure temperature
+    # need to be root to do this
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    base_dir = '/sys/bus/w1/devices/'
+    sensor_path_list = []
+    try:
+	for device_folder in glob.glob(base_dir + '28*'):
+	    sensor_path_list.append(device_folder + '/w1_slave')
+    except:
+	print "Error reading temperature sensor file, sensors connected?"
+	exit(1)  
+    sleep(3)
+    tempalert = TempAlert(sensor_path_list)
+    tempalert.start()
 
 if __name__ == "__main__":
     main()
